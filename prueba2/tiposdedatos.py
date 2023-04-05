ef83ba1b5da07980a48a13358d85ec3e7b7fd41e
Demian <53890358+demian35@users.noreply.github.com>
2023-04-05 02:28:58 -0600

    tipos de datos

---
+++ prueba2/tiposdedatos.py
@@ -0,0 +1,29 @@
+#Cadenas
+"String"
+'String'
+
+"""String
+    |dato1= 2
+     dato2= 4 
+"""
+
+'''String   
+     |dato1= 2
+    |dato2= 4 
+
+'''
+
+
+#numeros
+
+#int
+40
+
+#float
+40.2
+
+
+#booleanos
+
+True
+False
