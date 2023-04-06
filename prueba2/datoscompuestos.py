2d5d65c4ea92769d552dffadbd6b631b08549ebf
Demian <53890358+demian35@users.noreply.github.com>
2023-04-06 02:06:42 -0600

    creacion de datos compuestos tupla y lista

---
+++ prueba2/datoscompuestos.py
@@ -0,0 +1,19 @@
+#listas almacenan cualquier elemento
+lista1=["cadena","cadena2",True,False,4]
+
+
+#creamos una tupla de cuatro elementos
+tupla=("cadena","cadena2",True,False,4)
+
+
+#imprimimos lista
+print(lista1)
+
+#imprimimos elemento por el indidce donde esta almacenado un elemento
+print(lista1[4])
+
+#imprimimos la tupla
+print(tupla)
+
+#imprimimos un elemento de la tupla por su indice
+print(tupla[0])
