c4663589fd82fab01a4af0848945a3d38a97b7fb
Demian <53890358+demian35@users.noreply.github.com>
2023-04-06 01:28:22 -0600

    documentando practica

--- prueba2/variables.py
+++ prueba2/variables.py
@@ -1,12 +1,15 @@
+#definicion de variables cadenas y un int
 nombre=" Hola "
 edad=24
 felcitacion=" feliz cumple "
+
+#concatenacion de cadenas y uso de fstrings (casteo)
 cadena= nombre + f" Como estas {edad} " + felcitacion 
 
 
-
+#imprimimos concatenacion
 print(cadena)
 
-#operadores de pertenencia
+#operadores de pertenencia in /not in
 print("cumple" in cadena) 
 print("dem" not in cadena)
