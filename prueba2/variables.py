68224e3ab6503958a65f953515925871c023c16c
Demian <53890358+demian35@users.noreply.github.com>
2023-04-06 01:22:15 -0600

    jugando con variables , cadenas y pertenencias

---
+++ prueba2/variables.py
@@ -0,0 +1,12 @@
+nombre=" Hola "
+edad=24
+felcitacion=" feliz cumple "
+cadena= nombre + f" Como estas {edad} " + felcitacion 
+
+
+
+print(cadena)
+
+#operadores de pertenencia
+print("cumple" in cadena) 
+print("dem" not in cadena)
