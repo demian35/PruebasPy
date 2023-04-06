6bef0b65c65a436e93c613de186e999373cfc864
Demian <53890358+demian35@users.noreply.github.com>
2023-04-06 02:25:29 -0600

    CREACION Y MANIPULACION DE DICCIONARIO

--- prueba2/datoscompuestos.py
+++ prueba2/datoscompuestos.py
@@ -9,11 +9,37 @@ tupla=("cadena","cadena2",True,False,4)
 #imprimimos lista
 print(lista1)
 
+#cambiando el valor del elemento en el indice 4
+lista1[4]=True
 #imprimimos elemento por el indidce donde esta almacenado un elemento
 print(lista1[4])
+
+#imprimimos nueva lista
+print(lista1)
 
 #imprimimos la tupla
 print(tupla)
 
 #imprimimos un elemento de la tupla por su indice
 print(tupla[0])
+
+#tipo de dato conjunto (set) aqui no se puede acceder a los elementos por medio de indices ni se imprimen elementos repetidos
+conjunto={1,2,4,2,3,5}
+
+print(conjunto)
+
+#tipo de dato diccionaro
+diccionario={
+    'Nombre': "demian",
+    'Posicion': "Portero",
+    'Club': "Club Universidad AC",
+    'Edad': "36",
+    'Nacionalidad':"Mexico",
+    'Toma': "True"
+}
+
+#imprimimos el diccionario
+print(diccionario)
+
+#imprimimos el valor de la nacionalidad en el diccionario
+print(diccionario['Nacionalidad'])
