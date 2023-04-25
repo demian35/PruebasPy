4a5cc34eed26d20440230fac1786654706626075
Demian <53890358+demian35@users.noreply.github.com>
2023-04-24 19:36:11 -0600

    ejemplos de condicionales

---
+++ prueba4/condicionales.py
@@ -0,0 +1,14 @@
+
+usuario="demian35"
+usiarioValidado="demian35"
+password="2804jim23"
+passwordValidado="2804jim23"
+
+if usuario==usiarioValidado:
+    print("nombre de usuario correcto")
+if(password==passwordValidado):
+    print("contraseña correcta")
+    print("inicio de sesion correcto")
+else:
+    print("datos incorrectos")
+    
