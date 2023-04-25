653d44c2bb30ebd39b31177692a6c5fddd5ef9b3
Demian <53890358+demian35@users.noreply.github.com>
2023-04-24 20:43:53 -0600

    ejercicio 2 con variables

---
+++ Ejercicios1a4/ejercicio2variables.py
@@ -0,0 +1,11 @@
+print("introduzca dos numeros")
+
+print("Primer digito ")
+digito1=float(input())
+
+print("Segundo digito ")
+digito2=int(input())
+
+suma= digito1+digito2
+
+print(f"El resultado de la suma es {suma} ")
