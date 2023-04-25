90537fcfba257c2c8360e065e019deefe8137dc6
Demian <53890358+demian35@users.noreply.github.com>
2023-04-24 18:27:24 -0600

    comparadores de datos

---
+++ prueba3/comparadores.py
@@ -0,0 +1,17 @@
+#comparador para ver si dos elementos son iguales
+igualque= 4==3
+#operador mayor que
+mayorque= 4>=5
+
+#operador para ver que dos elementos son distintos
+distintoque= 4!=3
+
+print(mayorque)
+tipo=type(mayorque)
+print(tipo)
+
+print(distintoque)
+
+
+#ejemplo de chocolate 
+
