653dd291cf5e374d046bc959dbf884233b97a00d
Demian <53890358+demian35@users.noreply.github.com>
2023-04-24 18:00:21 -0600

    type

---
+++ prueba3/operadoresaritmeticos.py
@@ -0,0 +1,22 @@
+
+#Ej del uso del operador de suma y resta
+suma=2+4
+resta=4-2
+
+print(suma)
+print(resta)
+
+#operadores de multiplicacion, division,exp  y modulo
+
+mult=4*12
+div=20/4
+
+exp=2**3
+
+print(exp)
+print(div)
+print(mult)
+
+#type regresa el tipo de dato del dato que le pasan
+tipo=type(div)
+print(tipo)
