9b28b8df1bd729ef37bc2366616b5a46dc62fd99
Demian <53890358+demian35@users.noreply.github.com>
2023-04-24 21:34:35 -0600

    Ejercicio 5 aritmetica converitr °f a °C

---
+++ Ejercicios1a4/ejercicio5operadores.py
@@ -0,0 +1,9 @@
+print("convertidor de farenheit a centigrados")
+
+print("ingrese la temperatura en °f")
+
+temperatura=float(input())
+
+celsius=(temperatura-32)/1.8
+
+print(f"La temperatura en centigrados es {celsius} °C")
