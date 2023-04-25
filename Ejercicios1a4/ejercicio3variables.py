3957d5d2dd3899d5f92e9d2cd73baee50bd26c61
Demian <53890358+demian35@users.noreply.github.com>
2023-04-24 21:21:22 -0600

    ejercicio 3 variables y operadores aritmeticos

---
+++ Ejercicios1a4/ejercicio3variables.py
@@ -0,0 +1,19 @@
+print("introduzca tres numeros")
+
+print("Primer digito ")
+digito1=float(input())
+
+print("Segundo digito ")
+digito2=int(input())
+
+suma= digito1+digito2
+
+
+print(f"El resultado de la suma es {suma} ")
+
+print("introduzca un tercer numero")
+digito3=int(input())
+
+mult=suma*digito3
+
+print(f"El resultado de la multiplicacion de {suma} por {digito3} es {mult}")
