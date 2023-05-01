9597504a16c0115e58cc4d425f6b866471dc35d4
Demian <53890358+demian35@users.noreply.github.com>
2023-04-30 21:36:19 -0600

    ejercicio6 promedio y porcentaje

---
+++ Ejercicios1a4/ejercicio6.py
@@ -0,0 +1,9 @@
+print("Ingresa un número: ")
+numero=float(input())
+
+cantidad=15
+cantidad=15/100
+cantidad=numero*cantidad
+cantidad=numero-cantidad
+
+print(f"Descontando el 15% queda: {cantidad}")
---
+++ Ejercicios1a4/ejercicio6variables.py
@@ -0,0 +1,11 @@
+calif1=68
+calif2=78
+calif3=76
+calif4=72
+calif5=67
+
+
+sumacalif=calif1+calif2+calif3+calif4+calif5
+media=sumacalif/5
+
+print(f"media de cinco numeros {media}")
